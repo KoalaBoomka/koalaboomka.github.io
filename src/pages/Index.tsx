b5e3dba391365ba1978cf19c72ab44de92cf0e35
Dasha <amynul@gmail.com>
2025-11-06 12:39:46 +0100

    Update Index.tsx
    Bring Projects back in the structure

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
   return (
     <main className="min-h-screen">
       <Hero />
-      {/* <Projects /> */}
+      <Projects />
       <Experience />
       {/*<Blog /> */}
       <Contact />
